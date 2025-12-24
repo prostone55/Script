9131af3de227662986a72489ac64b417c07d2f15
prostone_ <les2potesdubled@gmail.com>
2025-12-24 17:23:13 +0100

    Update Site-43.lua

--- Site-43.lua
+++ Site-43.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/23c2b607d3143365f01fdcd044fd05fe16199cf3d20b9b8594d63f0d86ff7f4c/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/db0cfebe6024a34f422592c9886e6a72ee888a63aa743dcbd2f42ddc3b929d0d/download"))()
